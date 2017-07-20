d69392af704aa292ccc4475348b25ad80cde6b9f
Kolya Nosenko <kellbasss@gmail.com>
2017-07-20 15:22:06 +0300

    Landing Page Eclipse

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Clone Repo : `git clone https://github.com/KolyaNosenko/eclipce.git`
+2. Install node and bower vendors: `bower install` & `npm install`
+3. Build **/dist** folder to be able to review and work with a production version `gulp build`
---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".section_implementation_carousel").owlCarousel({
+        items:1,
+        loop:true,
+        margin:25
+    });
+    $(".section_services_carousel").owlCarousel({
+        items:1,
+        loop:true,
+        margin:25
+    })
+});
